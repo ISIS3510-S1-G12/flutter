537420055ac111bce118b31d126ee9b73c54922e
Maria Alejandra Londoño <m.londonoi@uniandes.edu.co>
2025-10-30 12:35:50 -0500

    ale bq

--- lib/views/pages/user/user_restaurant_detail_page.dart
+++ lib/views/pages/user/user_restaurant_detail_page.dart
@@ -15,6 +15,8 @@ import '/views/pages/user/user_ofertas_page.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
 
 class UserRestaurantDetailPage extends StatefulWidget {
   final Restaurant restaurant;
@@ -27,11 +29,21 @@ class UserRestaurantDetailPage extends StatefulWidget {
 
 class _UserRestaurantDetailPageState extends State<UserRestaurantDetailPage> {
   LatLng? _restaurantLocation; // 📍 ubicación del restaurante
+  final analytics = FirebaseAnalytics.instance;
 
   @override
   void initState() {
     super.initState();
     _loadRestaurantLocation();
+  }
+
+    @override
+  void dispose() {
+    FirebaseAnalytics.instance.logEvent(
+      name: 'screen_abandon',
+      parameters: {'restaurant_id': widget.restaurant.id},
+    );
+    super.dispose();
   }
 
   // 🔹 Geocodifica la dirección del restaurante actual
@@ -175,6 +187,10 @@ class _UserRestaurantDetailPageState extends State<UserRestaurantDetailPage> {
                                       Expanded(
                                         child: ElevatedButton.icon(
                                           onPressed: () async {
+                                            await analytics.logEvent(
+                                              name: 'bluetooth_click',
+                                              parameters: {'restaurant_id': restaurant.id},
+                                            );
                                             int peopleCount =
                                                 await vm.scanNearbyDevices();
                                             if (context.mounted) {
@@ -220,6 +236,10 @@ class _UserRestaurantDetailPageState extends State<UserRestaurantDetailPage> {
                                     width: double.infinity,
                                     child: ElevatedButton.icon(
                                       onPressed: () async {
+                                       await analytics.logEvent(
+                                          name: 'visited_click',
+                                          parameters: {'restaurant_id': restaurant.id},
+                                        ); 
                                         await visitVM
                                             .registerVisit(restaurant.id);
                                         if (context.mounted) {
