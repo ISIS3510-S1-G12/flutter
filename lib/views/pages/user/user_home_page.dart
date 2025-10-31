12cc73e9c8606e108385fe53b9350cb1d4b5f5da
AngelicaOrtizB <a.ortizb2@uniandes.edu.co>
2025-10-31 12:21:08 -0500

    commit

--- lib/views/pages/user/user_home_page.dart
+++ lib/views/pages/user/user_home_page.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
